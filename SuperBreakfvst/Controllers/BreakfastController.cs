5070dd955f2c7574703faa86c515354d750e8eb3
sfilipovski <49661929+sfilipovski@users.noreply.github.com>
2023-12-05 23:59:39 +0100

    add http requests

---
+++ SuperBreakfvst/Controllers/BreakfastController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperBreakfvst.Contracts.Breakfvst;
+
+namespace SuperBreakfvst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BreakfastController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateBreakfast(CreateBreakfastRequest request)
+        {
+
+            return Ok(request);
+        }
+
+        [HttpGet("{id:guid}")]
+        public IActionResult GetBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+
+        [HttpPut("{id:guid}")]
+        public IActionResult UpdateBreakfast(Guid id, UpdateBreakfastRequest request)
+        {
+
+            return Ok(request);
+        }
+
+        [HttpDelete("{id:guid}")]
+        public IActionResult DeleteBreakfast(Guid id)
+        {
+            return Ok(id);
+        }
+    }
+}
